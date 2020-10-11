9bd00ca5e3e63c86e09e770fd4baaedcaabb83bf
piotr <nwg.piotr@gmail.com>
2020-10-12 00:22:58 +0200

    Divide stuff into functions

--- src/gopsuinfo.go
+++ src/gopsuinfo.go
@@ -4,6 +4,7 @@ Fed up with learning just by reading the manual, I'll try to code my old psuinfo
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math"
 	"time"
@@ -11,20 +12,44 @@ import (
 	"github.com/shirou/gopsutil/cpu"
 )
 
-func main() {
-	var graph = []rune("_▁▂▃▄▅▆▇███")
-	var bar = ""
-
-	duration, _ := time.ParseDuration("400ms")
+func cpuGraph(delay *string) string {
+	bar := ""
+	graph := []rune("_▁▂▃▄▅▆▇███")
+	duration, _ := time.ParseDuration(*delay)
 	speeds, _ := cpu.Percent(duration, true)
 	for _, speed := range speeds {
 		bar += string(graph[int8(math.Round(speed/10))])
 	}
+	return bar
+}
+
+func cpuAvSpeed(delay *string) string {
+	duration, _ := time.ParseDuration(*delay)
 	avSpeed, _ := cpu.Percent(duration, false)
-	avs := fmt.Sprintf("%.1f", avSpeed[0])
-	if len(avs) < 4 {
+	avs := fmt.Sprintf("%.2f", avSpeed[0])
+	if len(avs) < 5 {
 		avs = " " + avs
 	}
-	var output = fmt.Sprintf("%s %s%%", bar, avs)
+	return fmt.Sprintf("%s%%", avs)
+
+}
+
+func main() {
+	componentsPtr := flag.String("C", "gatmd", `Components: (a)vg CPU load, (f)an speed, (g)rahical bar, (t)emperatures,
+	(m)emory, (u)ptime`)
+	cpuDelayPtr := flag.String("delay", "400ms", "CPU measurement delay [timeout]")
+	flag.Parse()
+
+	output := ""
+
+	for _, char := range *componentsPtr {
+		if string(char) == "g" {
+			output += cpuGraph(cpuDelayPtr) + " "
+		}
+		if string(char) == "a" {
+			output += cpuAvSpeed(cpuDelayPtr) + " "
+		}
+	}
+
 	fmt.Println(output)
 }
