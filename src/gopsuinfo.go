79a5a1b2db6632bc08966d5b6f572801ec66e0d8
piotr <nwg.piotr@gmail.com>
2020-10-13 03:50:32 +0200

    Glyphs ordered; uptime added

--- src/gopsuinfo.go
+++ src/gopsuinfo.go
@@ -13,29 +13,28 @@ import (
 	"github.com/shirou/gopsutil/host"
 )
 
-func cpuGraph(delay *string) string {
+func cpuGraph(g glyphs, delay *string) string {
 	bar := ""
-	graph := []rune("_▁▂▃▄▅▆▇███")
 	duration, _ := time.ParseDuration(*delay)
 	speeds, _ := cpu.Percent(duration, true)
 	for _, speed := range speeds {
-		bar += string(graph[int8(math.Round(speed/10))])
+		bar += string(g.graphCPU[int8(math.Round(speed/10))])
 	}
 	return bar
 }
 
-func cpuAvSpeed(delay *string) string {
+func cpuAvSpeed(g glyphs, delay *string) string {
 	duration, _ := time.ParseDuration(*delay)
 	avSpeed, _ := cpu.Percent(duration, false)
 	avs := fmt.Sprintf("%.2f", avSpeed[0])
 	if len(avs) < 5 {
 		avs = " " + avs
 	}
-	return fmt.Sprintf("%s%%", avs)
+	return fmt.Sprintf("%s%s%%", g.glyphCPU, avs)
 }
 
-func temperatures() string {
-	output := ""
+func temperatures(g glyphs) string {
+	output := g.glyphTemp
 	vals := make(map[string]int)
 	temps, _ := host.SensorsTemperatures()
 	for _, temp := range temps {
@@ -49,40 +48,65 @@ func temperatures() string {
 			vals["amdgpu"] = int(temp.Temperature)
 		}
 	}
-	v, exists := vals["k10temp"]
-	if exists {
+	if v, ok := vals["k10temp"]; ok {
 		output += fmt.Sprint(v)
 	} else {
-		v, exists := vals["acpitz"]
-		if exists {
+		if v, ok := vals["acpitz"]; ok {
 			output += fmt.Sprint(v)
 		}
 	}
-	v, exists = vals["amdgpu"]
-	if exists {
+	if v, ok := vals["amdgpu"]; ok {
 		output += "|" + fmt.Sprint(v)
 	}
 	output += "℃"
 	return output
 }
 
+func uptime(g glyphs) string {
+	output := g.glyphUptime
+	if t, e := host.Uptime(); e == nil {
+		hh := t / 3600
+		mm := t % 3600 / 60
+		output += fmt.Sprintf("%d:%d", hh, mm)
+	} else {
+		output += "??:??"
+	}
+	return output
+}
+
+// Settings for now will store glyphs only
+type glyphs struct {
+	graphCPU    []rune
+	glyphCPU    string
+	glyphTemp   string
+	glyphMem    string
+	glyphUptime string
+}
+
 func main() {
-	componentsPtr := flag.String("c", "gatmdu", `Output (c)omponents: (a)vg CPU load, (f)an speed, (g)rahical bar, (t)emperatures,
-	(m)emory, (u)ptime`)
-	cpuDelayPtr := flag.String("delay", "400ms", "CPU measurement delay [timeout]")
+	// Glyphs below may be replaced, e.g. "MEM:" instead of ""
+	g := glyphs{graphCPU: []rune("_▁▂▃▄▅▆▇███"), glyphCPU: "", glyphMem: "", glyphTemp: "", glyphUptime: " "}
+
+	componentsPtr := flag.String("c", "gatmdu",
+		`Output (c)omponents: (a)vg CPU load, (f)an speed, (g)rahical bar, (t)emperatures,
+		(m)emory, (u)ptime`)
+	cpuDelayPtr := flag.String("d", "500ms", "CPU measurement delay [timeout]")
 	flag.Parse()
 
 	output := ""
 
 	for _, char := range *componentsPtr {
 		if string(char) == "g" {
-			output += cpuGraph(cpuDelayPtr) + " "
+			output += cpuGraph(g, cpuDelayPtr) + " "
 		}
 		if string(char) == "a" {
-			output += cpuAvSpeed(cpuDelayPtr) + " "
+			output += cpuAvSpeed(g, cpuDelayPtr) + " "
 		}
 		if string(char) == "t" {
-			output += temperatures()
+			output += temperatures(g) + " "
+		}
+		if string(char) == "u" {
+			output += uptime(g) + " "
 		}
 	}
 
