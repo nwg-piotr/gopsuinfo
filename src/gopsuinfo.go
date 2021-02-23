7d8f87d6dd28c309c3d0ebe181659d9544c345bd
Piotr Miller <nwg.piotr@gmail.com>
2021-02-23 16:03:09 +0100

    icons

--- src/gopsuinfo.go
+++ src/gopsuinfo.go
@@ -31,18 +31,24 @@ func cpuGraph(g glyphs, delay *string) string {
 	return bar
 }
 
-func cpuAvSpeed(g glyphs, delay *string) string {
+func cpuAvSpeed(as_icon bool, g glyphs, delay *string) string {
 	duration, _ := time.ParseDuration(*delay)
 	avSpeed, _ := cpu.Percent(duration, false)
-	avs := fmt.Sprintf("%.2f", avSpeed[0])
-	if len(avs) < 5 {
+	avs := fmt.Sprintf("%.0f", math.Round(float64(avSpeed[0])))
+	if len(avs) < 2 {
 		avs = " " + avs
+	}
+	if as_icon {
+		return fmt.Sprintf("%s%%", avs)
 	}
 	return fmt.Sprintf("%s%s%%", g.glyphCPU, avs)
 }
 
-func temperatures(g glyphs) string {
-	output := g.glyphTemp
+func temperatures(as_icon bool, g glyphs) string {
+	output := ""
+	if !as_icon {
+		output += g.glyphTemp
+	}
 	vals := make(map[string]int)
 	temps, _ := host.SensorsTemperatures()
 	for _, temp := range temps {
@@ -70,8 +76,11 @@ func temperatures(g glyphs) string {
 	return output
 }
 
-func uptime(g glyphs) string {
-	output := g.glyphUptime
+func uptime(as_icon bool, g glyphs) string {
+	output := ""
+	if !as_icon {
+		output += g.glyphUptime
+	}
 	if t, e := host.Uptime(); e == nil {
 		hh := t / 3600
 		mm := t % 3600 / 60
@@ -82,8 +91,11 @@ func uptime(g glyphs) string {
 	return output
 }
 
-func memory(g glyphs) string {
-	output := g.glyphMem + " "
+func memory(as_icon bool, g glyphs) string {
+	output := ""
+	if !as_icon {
+		output += g.glyphMem + " "
+	}
 	stats, _ := mem.VirtualMemory()
 	used := math.Round(float64(stats.Used)) / 1048576
 	total := math.Round(float64(stats.Total)) / 1048576
@@ -107,7 +119,7 @@ func diskUsage(paths *string) string {
 		usage, _ := disk.Usage(path)
 		used := math.Round(float64(usage.Used)) / 1073741824
 		total := math.Round(float64(usage.Total)) / 1073741824
-		output += fmt.Sprintf("%s:%.1f/%.0f", path, used, total)
+		output += fmt.Sprintf("%s:%.1f/%.0f", path, used, total) + " "
 	}
 	output += "GiB"
 	return output
@@ -137,30 +149,56 @@ func main() {
 
 	componentsPtr := flag.String("c", "gatmnu",
 		`Output (c)omponents: (a)vg CPU load, (g)rahical CPU bar, disk usage by mou(n)tpoints, (t)emperatures, (m)emory, (u)ptime`)
-	cpuDelayPtr := flag.String("d", "450ms", "CPU measurement delay [timeout]")
+	iconPtr := flag.String("i", "", "Returns (i)con path and a single component (a, n, t, m, u) value")
+	cpuDelayPtr := flag.String("d", "900ms", "CPU measurement delay [timeout]")
 	pathsPtr := flag.String("p", "/", "Quotation-delimited, space-separated list of mou(n)tpoints")
+	setPtr := flag.Bool("dark", false, "Use dark icon set")
+
 	flag.Parse()
+	path := "/home/piotr/Obrazy/gopsuinfo/icons_light"
+	if *setPtr {
+		path = "/home/piotr/Obrazy/gopsuinfo/icons_dark"
+	}
 
 	output := ""
 
-	for _, char := range *componentsPtr {
-		if string(char) == "g" {
-			output += cpuGraph(g, cpuDelayPtr) + " "
+	if *iconPtr != "" {
+		if *iconPtr == "a" {
+			output += path + "/cpu.svg\n"
+			output += cpuAvSpeed(true, g, cpuDelayPtr)
+		} else if *iconPtr == "t" {
+			output += path + "/temp.svg\n"
+			output += temperatures(true, g)
+		} else if *iconPtr == "n" {
+			output += path + "/hdd.svg\n"
+			output += diskUsage(pathsPtr)
+		} else if *iconPtr == "m" {
+			output += path + "/mem.svg\n"
+			output += diskUsage(pathsPtr)
+		} else if *iconPtr == "u" {
+			output += path + "/up.svg\n"
+			output += uptime(true, g)
 		}
-		if string(char) == "a" {
-			output += cpuAvSpeed(g, cpuDelayPtr) + " "
-		}
-		if string(char) == "t" {
-			output += temperatures(g) + " "
-		}
-		if string(char) == "u" {
-			output += uptime(g) + " "
-		}
-		if string(char) == "m" {
-			output += memory(g) + " "
-		}
-		if string(char) == "n" {
-			output += diskUsage(pathsPtr) + " "
+	} else {
+		for _, char := range *componentsPtr {
+			if string(char) == "g" {
+				output += cpuGraph(g, cpuDelayPtr) + " "
+			}
+			if string(char) == "t" {
+				output += cpuAvSpeed(false, g, cpuDelayPtr) + " "
+			}
+			if string(char) == "t" {
+				output += temperatures(false, g) + " "
+			}
+			if string(char) == "u" {
+				output += uptime(false, g) + " "
+			}
+			if string(char) == "m" {
+				output += memory(false, g) + " "
+			}
+			if string(char) == "n" {
+				output += diskUsage(pathsPtr) + " "
+			}
 		}
 	}
 
